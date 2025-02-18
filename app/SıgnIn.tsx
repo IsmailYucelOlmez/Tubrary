f76f8ed629e352cf1bbb63ecd10ac33262ae755b
IsmailYucelOlmez <ismailyucelolmez514@gmail.com>
2025-02-18 20:53:30 +0300

    Initial Commit

---
+++ app/SıgnIn.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SıgnIn = () => {
+  return (
+    <View>
+      <Text>SıgnIn</Text>
+    </View>
+  )
+}
+
+export default SıgnIn
